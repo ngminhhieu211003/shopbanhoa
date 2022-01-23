d577777aa08821ddc7d0ec87462064d7104711cb
dongthd <huudong297@gmail.com>
2022-01-23 18:08:56 +0700

    create-template

--- src/main/java/vn/fs/GreenyShopApplication.java
+++ src/main/java/vn/fs/GreenyShopApplication.java
@@ -2,8 +2,9 @@ package vn.fs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class GreenyShopApplication {
 
 	public static void main(String[] args) {
--- src/main/java/vn/fs/controller/HomeController.java
+++ src/main/java/vn/fs/controller/HomeController.java
@@ -1,5 +1,15 @@
 package vn.fs.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
 
+	@GetMapping(value = "trang-chu")
+	public String home() {
+
+		return "web/home";
+
+	}
 }
