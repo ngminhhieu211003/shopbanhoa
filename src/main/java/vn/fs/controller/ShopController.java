1b593f92392271d96cf07d1b82cc72614122a86b
dongthd <huudong297@gmail.com>
2022-01-24 00:51:42 +0700

    commit-update code 24/1

--- src/main/java/vn/fs/controller/ShopController.java
+++ src/main/java/vn/fs/controller/ShopController.java
@@ -1,5 +1,14 @@
 package vn.fs.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ShopController {
 
+	@GetMapping(value = "/san-pham")
+	public String shop() {
+		
+		return "web/shop";
+	}
 }
