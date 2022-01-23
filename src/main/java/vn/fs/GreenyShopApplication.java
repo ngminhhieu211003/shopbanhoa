59162e511268b3974a794d3c707d49c7a3279f61
dongthd <huudong297@gmail.com>
2022-01-23 17:37:35 +0700

    commit

---
+++ src/main/java/vn/fs/GreenyShopApplication.java
@@ -0,0 +1,13 @@
+package vn.fs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenyShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenyShopApplication.class, args);
+	}
+
+}
