9e44ba22f0621daf22eb7d33a943300827a205c4
dongthd <huudong297@gmail.com>
2022-01-25 18:40:41 +0700

    Commit

---
+++ src/main/java/vn/fs/service/SendMailService.java
@@ -0,0 +1,17 @@
+package vn.fs.service;
+
+import java.io.IOException;
+
+import javax.mail.MessagingException;
+
+import vn.fs.model.MailInfo;
+
+public interface SendMailService {
+	void run();
+
+	void queue(String to, String subject, String body);
+
+	void queue(MailInfo mail);
+
+	void send(MailInfo mail) throws MessagingException, IOException;
+}
