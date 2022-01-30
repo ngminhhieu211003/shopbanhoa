c7758dd3d0406dc16a0656f17686070c975e3237
dongthd <huudong297@gmail.com>
2022-01-31 00:51:24 +0700

    Commit- update code

--- src/main/java/vn/fs/repository/OrderRepository.java
+++ src/main/java/vn/fs/repository/OrderRepository.java
@@ -1,9 +1,6 @@
 package vn.fs.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import vn.fs.entities.Order;
